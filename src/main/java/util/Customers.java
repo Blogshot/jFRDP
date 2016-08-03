59a378abe559a6c6c23c2809bafc8d1e594b982e
Blogshot <knottsascha@gmail.com>
2016-08-03 10:01:38 +0200

    Created wrapper-class to add custom getter and contains()-methods

---
+++ src/main/java/util/Customers.java
@@ -0,0 +1,30 @@
+package util;
+
+import main.Customer;
+
+import java.util.ArrayList;
+
+public class Customers extends ArrayList<Customer> {
+
+  public boolean contains(String name) {
+
+    for (Customer customer : this) {
+      if (customer.getName().equals(name)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public Customer get(String name) {
+
+    for (Customer customer : this) {
+      if (customer.getName().equals(name)) {
+        return customer;
+      }
+    }
+
+    return null;
+  }
+}
