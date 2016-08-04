2be4a6aaaf64d80dc0a86fd81dbb0c2bd22d8428
Blogshot <knottsascha@gmail.com>
2016-08-04 13:47:05 +0200

    Cleanup

--- src/main/java/util/Customers.java
+++ src/main/java/util/Customers.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 @SuppressWarnings("SameParameterValue")
 public class Customers extends ArrayList<Customer> {
 
+  @SuppressWarnings("BooleanMethodIsAlwaysInverted")
   public boolean contains(String name) {
 
     for (Customer customer : this) {
