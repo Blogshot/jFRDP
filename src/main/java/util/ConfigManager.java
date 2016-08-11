9a7271e4e7c743e9fd5f57c49bdf83b6060a8806
Blogshot <knottsascha@gmail.com>
2016-08-11 16:08:56 +0200

    Fixed reaction to empty names

--- src/main/java/util/ConfigManager.java
+++ src/main/java/util/ConfigManager.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import main.Connection;
 import main.Customer;
+import util.Dialogs.ErrorDialog;
 
 import javax.swing.*;
 import java.io.*;
@@ -15,13 +16,13 @@ import static main.MainForm.secrethash;
 import static util.MD5Wrapper.createHash;
 
 public class ConfigManager {
-
+  
   public static void loadConfig() {
-
+    
     try {
       Reader reader = new FileReader("config.json");
-
-
+      
+      
       BufferedReader br = new BufferedReader(reader);
       StringBuilder sb = new StringBuilder();
       String s;
@@ -29,56 +30,59 @@ public class ConfigManager {
         sb.append(s);
       }
       reader.close();
-
+      
       String content = sb.toString();
-
+      
       JsonElement root = new JsonParser().parse(content);
-
+      
       master = root.getAsJsonObject().get("master").getAsString();
-
+      
     } catch (Exception e) {
       JOptionPane.showMessageDialog(new JFrame(), e.getMessage());
       e.printStackTrace();
       new ErrorDialog(e);
-
+      
     }
   }
-
+  
   public static void saveConfig() {
     saveConfig("config.json");
   }
-
+  
   public static void saveConfig(String name) {
     System.out.print("Saving configuration...");
-
-    try {
-
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-      JsonObject json = new JsonObject();
-
-      json.addProperty("master", master);
-
-      FileWriter writer = new FileWriter(name);
-      writer.write(gson.toJson(json));
-      writer.close();
-
-      System.out.println("done.");
-
-    } catch (IOException e) {
-      e.printStackTrace();
-      new ErrorDialog(e);
-
+    
+    if (name.equals("")) {
+      System.out.print("Invalid name, aborting.");
+      
+    } else {
+      try {
+        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        
+        JsonObject json = new JsonObject();
+        
+        json.addProperty("master", master);
+        
+        FileWriter writer = new FileWriter(name);
+        writer.write(gson.toJson(json));
+        writer.close();
+        
+        System.out.println("done.");
+        
+      } catch (IOException e) {
+        e.printStackTrace();
+        new ErrorDialog(e);
+        
+      }
     }
-
-
   }
-
+  
   public static Customers loadCustomers() {
-
+    
     try {
       Reader reader = new FileReader("connections.json");
-
+      
       BufferedReader br = new BufferedReader(reader);
       StringBuilder sb = new StringBuilder();
       String s;
@@ -86,82 +90,96 @@ public class ConfigManager {
         sb.append(s);
       }
       reader.close();
-
+      
       String content = sb.toString();
-
+      
       JsonElement root = new JsonParser().parse(content);
-
-      Type type = new TypeToken<Customers>() {}.getType();
-
+      
+      Type type = new TypeToken<Customers>() {
+      }.getType();
+      
       return new Gson().fromJson(root, type);
-
+      
     } catch (Exception e) {
       e.printStackTrace();
       new ErrorDialog(e);
     }
-
+    
     System.out.println("No connections found, returning empty array");
     return new Customers();
   }
-
+  
   public static void saveConnections() {
-
+    
     saveConnections("connections.json");
   }
-
+  
   public static void saveConnections(String name) {
     System.out.print("Saving connections...");
-
-    try {
-
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-      String json = gson.toJson(customers);
-      FileWriter writer = new FileWriter(name);
-      writer.write(json);
-      writer.close();
-
-      System.out.println("done.");
-
-    } catch (IOException e) {
-      e.printStackTrace();
-      new ErrorDialog(e);
-    }
-  }
-
-  public static void setNewMaster(String newMaster) {
-
-    String newHash = createHash(newMaster);
-
-    System.out.println("Old hash: " + master);
-    System.out.println("New hash: " + newHash);
-
-    for (Customer cust : customers) {
-
-      System.out.println("Re-Encrypting " + cust.name + "...");
-
-      for (Connection connection : cust.connections) {
-
-        System.out.println("  Re-Encrypting " + connection.label + "...");
-
-        // Decrypt password
-        String clearPass = connection.decrypt(connection.password);
-
-        // Re-Encrypt with new key
-        connection.encrypt(clearPass, newHash);
-
-        System.out.println("    New encrypted txt_password: " + connection.password);
+    
+    if (name.equals("")) {
+      System.out.print("Invalid name, aborting.");
+    } else {
+      try {
+        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        
+        String json = gson.toJson(customers);
+        FileWriter writer = new FileWriter(name);
+        writer.write(json);
+        writer.close();
+        
+        System.out.println("done.");
+        
+      } catch (IOException e) {
+        e.printStackTrace();
+        new ErrorDialog(e);
       }
     }
-
-    System.out.println("Saving new Hash");
-
+  }
+  
+  public static void setNewMaster(String newMaster) {
+    
+    // Create hash of new master key
+    String newHash = createHash(newMaster);
+    
+    for (Customer cust : customers) {
+      
+      for (Connection connection : cust.connections) {
+        
+        if (!connection.password.equals("")) {
+          // Decrypt password
+          String clearPass = connection.decrypt(connection.password);
+          
+          if (newMaster.equals("")) {
+            // Do not encrypt
+            connection.password = clearPass;
+          } else {
+            // Re-Encrypt with new key
+            System.out.println("  Re-Encrypting " + connection.label + " with hash: " + newHash);
+            
+            System.out.println("  Old Pass/Pass-Hash " + connection.password);
+            
+            connection.password = connection.encrypt(clearPass, newHash);
+            
+            System.out.println("  New Pass-Hash " + connection.password);
+          }
+        }
+      }
+    }
+    
     // Set new Hash
-    secrethash = newHash;
-    master = newHash;
-
+    if (newMaster.equals("")) {
+      // mark master as disabled
+      secrethash = "disabled";
+      master = "disabled";
+    } else {
+      System.out.println("Saving new Hash");
+      secrethash = newHash;
+      master = newHash;
+    }
+    
     saveConfig();
     saveConnections();
   }
-
 }
