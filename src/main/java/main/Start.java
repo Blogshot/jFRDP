ebcb293ee3d205b16163d7860230e21b83b921b2
Blogshot <knottsascha@gmail.com>
2016-08-11 16:06:56 +0200

    Placed InputDialog in own Class

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -14,14 +14,4 @@ public class Start {
   public static String showSecretInputDialog() {
     return JOptionPane.showInputDialog(new JFrame(), "Enter your master-key to unlock passwords");
   }
-
-  public static String showInputDialog(String text) {
-
-    // prompt the user to enter their master-key
-
-    // get the user's input. note that if they press Cancel, 'secret' will be null
-    return JOptionPane.showInputDialog(new JFrame(), text);
-  }
-
-
 }
