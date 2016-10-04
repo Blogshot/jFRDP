6c01d85afc4b18096edb9ba188ae53bdae35031a
Blogshot <knottsascha@gmail.com>
2016-10-04 15:17:08 +0200

    Added note for brew-install
    Former-commit-id: 2851058147ef31ac7d8fc0e3fc28d66929a09fa3

--- includeXFreeRDP.sh
+++ includeXFreeRDP.sh
@@ -4,4 +4,7 @@
 #
 # Does not work with IntelliJ because it isn't designed
 # to be run as root. Run manually instead.
+
+# `brew install homebrew/x11/freerdp --HEAD`
+
 sudo cp $(which xfreerdp) $(pwd)/src/main/resources/xfreerdp
